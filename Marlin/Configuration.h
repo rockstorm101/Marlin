5bd7234d10ae428c275b971153b43e9c2bc309d4
Rock Storm <rockstorm@gmx.com>
2017-02-11 19:33:08 +0100

    Reduce maximum Z axis position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -338,7 +338,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define X_MIN_POS 0
 #define Y_MAX_POS 180
 #define Y_MIN_POS 0
-#define Z_MAX_POS 180
+#define Z_MAX_POS 130
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
